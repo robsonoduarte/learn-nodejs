4c3ce1d2dfb0b9b909d5603da9101beaf6dd9c63
Robson Duarte <robson.o.d@gmail.com>
2016-09-12 19:47:02 -0300

    add download file and stream write

--- async/process.js
+++ async/process.js
@@ -4,8 +4,23 @@ var sync = require('./read-sync')
 var async = require('./read-async')
 
 
-sync()
-async()
+var file = "node.js.tar.xz"
+var stream = fs.createWriteStream(file)
+
+
+http.get("http://nodejs.org/dist/v4.5.0/node-v4.5.0-linux-x64.tar.xz",function(rsp){
+
+	rsp.on('data',function(data){
+	    stream.write(data)				
+	});
+
+	rsp.on('end',function(){
+	   stream.end()
+	   async(file)	
+	   sync(file)
+	});
+});
+
 
 
 
