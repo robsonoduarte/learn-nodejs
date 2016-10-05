f5464fc033bdcde3f83615c91d09229189f68e4a
Robson Duarte <robson.o.d@gmail.com>
2016-10-05 20:30:07 -0300

    add new structure in project

--- node-talk/app.js
+++ node-talk/app.js
@@ -1,15 +1,19 @@
-var express = require('express');
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var app = express();
+var express = require('express')
+	, load = require('express-load')
+	, app = express();
 
 
 // view engine setup
 app.set('views', __dirname, '/views');
 app.set('view engine', 'ejs');
 //app.use(express.static(__dirname, '/public'));
-app.use('/', routes);
-app.use('/users', users);
+
+
+load('models')
+	.then('controllers')
+	.then('routes')
+	.into(app);
+
 
 app.listen(3000,function(){
 	console.log('The System is running')
