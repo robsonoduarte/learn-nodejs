a20271e4107b2355b99ab6f580518935059ed068
Robson Duarte <robson.o.d@gmail.com>
2016-10-05 21:26:23 -0300

    add new routes and fix the problem of view dir

--- node-talk/app.js
+++ node-talk/app.js
@@ -4,9 +4,9 @@ var express = require('express')
 
 
 // view engine setup
-app.set('views', __dirname, '/views');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-//app.use(express.static(__dirname, '/public'));
+app.use(express.static(__dirname + '/public'));
 
 
 load('models')
