73ceea7aa08fcac78b413b77f2541062ab759dd3
robsonoduarte <robson.o.d@gmail.com>
2023-11-23 22:03:26 -0300

    :done:

--- async/read-async.js
+++ async/read-async.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
+const bs = require("byte-size");
 
 const read = function (file) {
 	const start = new Date();
-	fs.readFile(file, () => {})
-	console.info('Total Time in async %dms', new Date() - start)
+	fs.readFile(file, (_, buffer) => {
+		const data = bs(buffer.length)
+		const total = new Date() - start
+		console.info(`Read file size ${data.value}${data.unit} sync in ${total}ms`)
+	})
 };
 
 module.exports = read
--- async/read-sync.js
+++ async/read-sync.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
+const bs = require("byte-size")
 
 const read = function (file) {
-	const start = new Date();
-	fs.readFileSync(file)
-	console.info('Total Time in sync %dms', new Date() - start)
+	const start= new Date();
+	const data = bs(fs.readFileSync(file).length)
+	const total = new Date() - start
+	console.info(`Read file size ${data.value}${data.unit} sync in ${total}ms`)
 };
 
 module.exports = read
